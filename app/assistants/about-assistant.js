470e298a3c9fdf07340d0fddc76055e54beeb0ba
George Mari <george@mari1938.org>
2018-10-12 19:13:47 -0500

    Make adjustments to wording in preferences and about scenes.

--- app/assistants/about-assistant.js
+++ app/assistants/about-assistant.js
@@ -6,8 +6,8 @@ AboutAssistant.prototype = {
 
 //setup localized text and variables
 $("AppTitleText").innerHTML = $L(Mojo.Controller.appInfo.title);
-$("AppVersionText").innerHTML = Mojo.Controller.appInfo.version + ", " + $L("by") + " Jan Heřman (72ka) - (API v" + google.maps.version + ")";
-$("AppDescription").innerHTML = $L("This application is trying to be a replacement of unsupported Google Maps application for WebOS devices. The application is based on the Google Maps API V3. Do you like this application? If do, you can support me in development. Thank you!");
+$("AppVersionText").innerHTML = Mojo.Controller.appInfo.version + ", " + $L("by") + " George Mari - (API v" + google.maps.version + ")";
+$("AppDescription").innerHTML = $L("This application is trying to be a replacement of unsupported Google Maps application for WebOS devices.");
 $("ChangelogText").innerHTML = $L("Changelog");
 $("DescriptionText").innerHTML = $L("Description");
 $("ContributorsText").innerHTML = $L("Contributors");
